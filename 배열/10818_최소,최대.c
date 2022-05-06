1620146ae837e582e722b54606c8538dc5aff476
goeun-oh <qw433812@gmail.com>
2022-05-07 01:13:33 +0900

    addit code

---
+++ 배열/10818_최소,최대.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int N, i, A;
+	int min = 1000001, max = -10000001;
+	
+	scanf("%d", &N);
+	
+
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &A);
+		
+		if (A > max)
+			max = A;
+		if (A < min)
+			min = A;
+	}
+
+	printf("%d %d", min, max);
+}
